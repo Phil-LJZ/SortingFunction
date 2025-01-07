f4b405bedf3a8e9582cf0d56c325aad542e4a068
Phil-LJZ <107176302+Phil-LJZ@users.noreply.github.com>
2025-01-07 17:43:47 +0800

    Add files via upload

---
+++ sort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "sort.h"
+
+void sort(int *, int);
+void print_array(int *, int, int);
+
+int main(void)
+{
+    int array[5] = {1, 1, 1, 2, 2};
+
+    sort(array, 5);
+    printf("done!");
+    // printf("{");
+    // for (int i = 0; i < 9; i++)
+    // {
+    //     printf("%d, ", array[i]);
+    // }
+    // printf("%d}", array[9]);
+    return 0;
+}
+
+void print_array(int * array, int size, int index)
+{
+    printf("(%.2d)  {", index);
+    for (int i = 0; i < size - 1; i++)
+    {
+        printf("%d, ", array[i]);
+    }
+    printf("%d}\n", array[9]);
+}
---
+++ sort.h
@@ -0,0 +1,58 @@
+#include <stdbool.h>
+
+void print_array(int *, int, int);
+
+// void sort(int * array, int size)
+// {
+//     int p;
+
+//     for (int a = 0; a < size; a++)
+//     {
+//         for (int b = 0; b < (size - 1); b++)
+//         {
+//             if (array[b] < array[b + 1])
+//             {
+//                 p = array[b];
+//                 array[b] = array[b + 1];
+//                 array[b + 1] = p;
+//             }
+//         }
+//         print_array(array, size, a);
+//     }
+// }
+
+void sort(int * array, int size)
+{
+    int p;
+    bool sorting_completed = false;
+    int sorted_number = 0;
+    int index = 0;
+    int count = 0;
+
+    while (!sorting_completed)
+    {
+        for(index = 0, sorted_number = 0; index < size - 1; index++)
+        {
+            if (array[index] < array[index + 1])
+            {
+                p = array[index];
+                array[index] = array[index + 1];
+                array[index + 1] = p;
+            }
+            if (array[size - 1 - index - 1] <= array[size - 1 - index])
+            {
+                sorted_number--;
+            }
+            else
+            {
+                sorted_number++;
+            }
+            if (sorted_number == (size - 1))
+            {
+                sorting_completed = true;
+            }
+        }
+        print_array(array, size, count);
+        count++;
+    }
+}
